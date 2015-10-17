36ad5ca7f472a6618eed161a8cdeb149b66a5907
David Bailey <david.bailey@archlinux.net>
2015-10-17 21:57:28 +0100

    Update user agent and add footer text

--- bot.py
+++ bot.py
@@ -4,7 +4,10 @@ import praw
 import scraper
 from scraper import article
 
-r = praw.Reddit('BBCJapanese Bot by /u/magicpurpledinosaur. '
+footer = ("\n\n---\n\nI'm a bot! Bleep bloop. You can find my source code "
+          "[here](https://github.com/tech4david/bbcjapanesebot).")
+
+r = praw.Reddit('/r/BBCJapanese Bot by /u/magicpurpledinosaur. '
                 'GitHub: https://github.com/tech4david/bbcjapanesebot')
 r.login()
 already_done = []
@@ -14,6 +17,6 @@ while True:
 
     if not article.id in already_done:
         already_done.append(article.id)
-        r.submit('bbcjapanesetest', article.title, text=scraper.text)
+        r.submit('bbcjapanesetest', article.title, text=scraper.text + footer)
 
         time.sleep(10 * 60)
